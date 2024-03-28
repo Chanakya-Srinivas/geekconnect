7783324b21bc9129fc498d147b5cff968a98734d
Chanakya-Srinivas <chanu.korada98@gmail.com>
2024-03-28 13:08:13 -0500

    first commit

---
+++ src/main/java/com/studentassist/geekconnect/GeekconnectApplication.java
@@ -0,0 +1,13 @@
+package com.studentassist.geekconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studentassist/geekconnect/GeekconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentassist.geekconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
